6aee846f1721517784c473f5af1f8906a5d880a0
Tom Szwaja <th.szwaja@gmail.com>
2023-06-06 13:37:39 +0200

    feat: set up and test i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx(), image()],
+	integrations: [tailwind(), mdx(), image(), astroI18next()],
 });

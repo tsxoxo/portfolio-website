9bdebf3a629c9396bb6701ea3635e462aa9a5cdf
Tom Szwaja <th.szwaja@gmail.com>
2023-09-05 09:31:08 +0200

    add markdown-toc, accessible emojis

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
 import image from '@astrojs/image'
 import mdx from '@astrojs/mdx'
+import remarkToc from 'remark-toc'
+import a11yEmoji from '@fec/remark-a11y-emoji'
 import tailwind from '@astrojs/tailwind'
 import astroI18next from 'astro-i18next'
 
@@ -13,5 +15,9 @@ export default defineConfig({
 			serviceEntryPoint: '@astrojs/image/sharp'
 		}),
 		astroI18next()
-	]
+	],
+	markdown: {
+		// Applied to .md and .mdx files
+		remarkPlugins: [remarkToc, a11yEmoji]
+	}
 })

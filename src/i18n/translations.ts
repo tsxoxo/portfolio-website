668d2162fac2686fe55beab7bf4585bce01a85b2
Tom Szwaja <59713582+tsxoxo@users.noreply.github.com>
2025-02-08 12:19:39 +0000

    Basic i18n setup as per Astro 5 guide

---
+++ src/i18n/translations.ts
@@ -0,0 +1,24 @@
+export const languages = {
+    en: 'English',
+    de: 'Deutsch',
+};
+
+export const defaultLang = 'en';
+
+export const translations = {
+    en: {
+        "meta.title": "Tom Szwaja | Web Person",
+        "meta.description": "Here is the description of my awesome website!",
+
+        "nav.contactButton": "Contact me",
+
+        "hero.heading": "Hi, I'm Tom!",
+        "hero.subheading": "Making the web friendlier, one website at a time.",
+        "hero.what-paragraphs": "<0><p>I design user experiences,</p><p>build websites,</p><p>and help people maintain them.</p></0>",
+        "hero.what-raw": "I design user experiences,  build websites and help people maintain them.",
+        "hero.cta": "What can I do for you?"
+
+    },
+    de: {
+    },
+} as const;
